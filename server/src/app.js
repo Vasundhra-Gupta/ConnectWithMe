9bd498da55f0d2908bfb426f5e57e600ffe9dc48
vasundhra gupta <vasundhragupta962@gmail.com>
2025-05-29 19:11:23 +0530

    redeploying on render

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false })); // for form data
 app.use(cookieParser()); // to parse cookie
 app.use(
     cors({
-        origin: [ "http://localhost:5173", "https://connectwithme-vatb.onrender.com"],
+        origin: [ "http://localhost:5173", "https://connectwithme-vatb.onrender.com, https://connect-with-me-umber.vercel.app/"],
         credentials: true,
     })
 );
